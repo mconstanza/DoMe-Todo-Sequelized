05b45af57f7518c4c95b18e7074e7a0008d3a120
Michael Constanza <Michael Constanza>
2016-11-13 18:49:15 -0500

    changed production db info

---
+++ models/user.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING
+  },{
+    classMethods: {
+      associate: function(models){
+        User.hasMany(models.Todo, {as: 'Todos'});
+      }
+    }
+  })
+  return User;
+}
